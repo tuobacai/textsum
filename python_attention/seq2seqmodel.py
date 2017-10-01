2b938f2591e496b86db15f98ec8630ec3e42e41b
yingjie10 <yingjie10@staff.weibo.com>
2017-10-02 02:41:00 +0800

    update

--- python_attention/seq2seqmodel.py
+++ python_attention/seq2seqmodel.py
@@ -124,12 +124,7 @@ class Seq2SeqModel(object):
                 self.inputs.append(input_plhd)
                 self.inputs_embed.append(input_embed)
 
-        def lstm_encode_cell():
-            cell = tf.contrib.rnn.LSTMCell(size, state_is_tuple=True)
-            cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=self.dropoutRate)
-            return cell
-
-        def lstm_decode_cell():
+        def lstm_cell():
             cell = tf.contrib.rnn.LSTMCell(size, state_is_tuple=True)
             cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=self.dropoutRate)
             return cell
@@ -137,20 +132,22 @@ class Seq2SeqModel(object):
         # LSTM
         with tf.device(devices[1]):
             # for encoder
-            if num_layers == 1:
-                encoder_cell = lstm_encode_cell()
-            else:
-                encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_encode_cell() for _ in xrange(num_layers)],
-                                                           state_is_tuple=True)
-            encoder_cell = tf.contrib.rnn.DropoutWrapper(encoder_cell, output_keep_prob=self.dropoutRate)
+            with tf.variable_scope("encoder_cell"):
+                if num_layers == 1:
+                    encoder_cell = lstm_cell()
+                else:
+                    encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in xrange(num_layers)],
+                                                               state_is_tuple=True)
+                encoder_cell = tf.contrib.rnn.DropoutWrapper(encoder_cell, output_keep_prob=self.dropoutRate)
 
             # for decoder
-            if num_layers == 1:
-                decoder_cell = lstm_decode_cell()
-            else:
-                decoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_decode_cell() for _ in xrange(num_layers)],
-                                                           state_is_tuple=True)
-            decoder_cell = tf.contrib.rnn.DropoutWrapper(decoder_cell, output_keep_prob=self.dropoutRate)
+            with tf.variable_scope("decoder_cell"):
+                if num_layers == 1:
+                    decoder_cell = lstm_cell()
+                else:
+                    decoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in xrange(num_layers)],
+                                                               state_is_tuple=True)
+                decoder_cell = tf.contrib.rnn.DropoutWrapper(decoder_cell, output_keep_prob=self.dropoutRate)
 
         self.encoder_cell = encoder_cell
         self.decoder_cell = decoder_cell
